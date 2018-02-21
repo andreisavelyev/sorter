228c83d3c8b24751ccdf1a1b9c273b4f15ee03a3
raikaat <raikaat@gmail.com>
2018-02-21 21:01:31 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = [];
+    this.temp=[];
+    this.compareFunction = function(a,b){
+      return a-b;
+    }
   }
 
   add(element) {
+    this.arr.push(element);
     // your implementation
   }
 
   at(index) {
-    // your implementation
+      return this.arr[index];// your implementation
   }
 
   get length() {
-    // your implementation
+      return this.arr.length;// your implementation
   }
 
   toArray() {
-    // your implementation
+    return this.arr;// your implementation
   }
 
   sort(indices) {
+
+    for(var i = 0; i < indices.length; i++){
+      this.temp[i] = this.arr[indices[i]];
+    };
+
+    this.temp.sort(this.compareFunction);
+
+    indices=indices.sort(function(a, b){
+      return a-b;
+    });
+
+    for (var j = 0; j<indices.length; j++){
+      this.arr[indices[j]]=this.temp[j];
+    };
     // your implementation
   }
 
   setComparator(compareFunction) {
+    this.compareFunction=compareFunction;
     // your implementation
   }
 }
